34423ab6fa90c57c2e082a5e77dff14f5b086077
Clement ROULLET <clement.roullet@gmail.com>
2012-06-24 21:50:47 +0200

    Added validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,10 +2,21 @@ class Contact < ActiveRecord::Base
   has_many :emails
   #belongs_to :country
 
-  accepts_nested_attributes_for :emails, allow_destroy: true
+  accepts_nested_attributes_for :emails, allow_destroy: true #, :reject_if => proc {|a| a["address"].blank?}
 
   attr_accessible :address, :category, :cell, :company, :country, :fax,
     :first_name, :infos, :is_active, :is_ceres_member, :last_name,
     :position, :postal_code, :telphone, :emails_attributes
+
+  validates_associated :emails
+  validates_presence_of :company, :country
+
+  #validate :something
+
+  def something
+    Rails.logger.debug(self)
+    errors[:base] << "C'est la merde"
+    errors.add(:emails, "Tututu")
+  end
 
 end
--- app/models/email.rb
+++ app/models/email.rb
@@ -1,4 +1,7 @@
 class Email < ActiveRecord::Base
-  belongs_to :contact
+  belongs_to :contact  
   attr_accessible :address, :contact_id, :has_bounced
+
+  validates_uniqueness_of :address
+  validates_presence_of :address
 end
