5318d20758900a60c0a4486b2cf64bb3be010d6b
Clement ROULLET <clement.roullet@gmail.com>
2012-06-25 14:27:02 +0200

    enabled registerable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable, :registerable
 
   # :registerable, :recoverable,
 
