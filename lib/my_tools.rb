b9a664df6b1c819e2ba81af05ea5cd5387d1fcc5
Clement ROULLET <clement.roullet@gmail.com>
2012-08-02 13:27:55 +0200

    Added Friendly username function to generate membernames from Ajax call

--- lib/my_tools.rb
+++ lib/my_tools.rb
@@ -1,13 +1,44 @@
+#encoding: utf-8
 module MyTools
 
-  # Returns a unique hash suitable for obfuscating the URL of an otherwise
+  # this turns a unique hash suitable for obfuscating the URL of an otherwise
   # publicly viewable attachment.
-  def hash_hash(hash_data, hash_digest = "SHA1", hash_secret = "NSWGbjMdPUAt2XV6hyJCKDgwQzi73vBe8Rlcmf95FpLk4YuxrHqZoaTEns")
-    #raise ArgumentError, "Unable to generate hash without :hash_secret" unless @hash_secret
+  def hash_hash(hash_data, hash_digest = "SHA1", hash_secstr = "NSWGbjMdPUAt2XV6hyJCKDgwQzi73vBe8Rlcmf95FpLk4YuxrHqZoaTEns")
+    #raise ArgumentError, "Unable to generate hash without :hash_secstr" unless @hash_secstr
     require 'openssl' unless defined?(OpenSSL)
-    OpenSSL::HMAC.hexdigest(OpenSSL::Digest.const_get(hash_digest).new, hash_secret, hash_data)
+    OpenSSL::HMAC.hexdigest(OpenSSL::Digest.const_get(hash_digest).new, hash_secstr, hash_data)
   end
-  def hash(a = 1, b = 2)
-    a + b
+
+  def friendly_user_name str
+    begin
+      str.strip
+      #blow away apostrophes
+      str.gsub! /['`]/,""
+      # @ --> at, and & --> and
+      str.gsub! /@/, " at "
+      str.gsub! /&/, " and "
+      str.gsub! /\b\.\b/, ""
+      str.gsub! /\bgmbh|ltd|corp|sarl|sas|sl|sa|inc\b/i, ""
+      Hash[*%w(ä a Ä A ö o Ö O ü u Ü U ß B é e è e à a ù u ë e ê e î i û u â a ç c')].each_pair do |k,v|
+        str.gsub!(/#{k}/, v)
+      end
+      #replace all non alphanumeric, underscore or periods with underscore
+      str.gsub! /\s*[^A-Za-z0-9\-\.\-]\s*/, '_'
+      #convert double underscores to single
+      str.gsub! /_+/,"_"
+      #strip off leading/trailing underscore
+      str.gsub! /\A[_\.]+|[_\.]+\z/,""
+    rescue
+      str = "An error occured, please generate a new username"
+    end
+    str.downcase
   end
+  module_function :friendly_user_name
+
+  def generate_random_string len
+    require 'digest/sha1'
+    Digest::SHA1.hexdigest(Time.now.to_s.split(//).sort_by{rand}.join)[0..len]
+  end
+  module_function :generate_random_string
+
 end
