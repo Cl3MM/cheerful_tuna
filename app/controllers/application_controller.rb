0b5570d110e327892f17e75d8d0ab31f8eb657ed
Clement ROULLET <clement.roullet@gmail.com>
2012-08-05 01:26:16 +0200

    Added url after sign in/out redirect user and members

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,33 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def redirect_user!
+    if current_user
+      flash[:error] = "Sorry, this section is for Members only !"
+      redirect_to root_path
+    end
+  end
+
+  def redirect_member!
+    if current_member
+      flash[:error] = "Sorry, this section is closed to members"
+      redirect_to profile_path
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(Member)
+      members_profile_path
+    else
+      super
+    end
+  end
+
+  def after_sign_out_path_for(resource)
+    if resource.to_s == "member"
+      new_member_session_path
+    else
+      super
+    end
+  end
 end
