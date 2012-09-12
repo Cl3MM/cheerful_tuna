bb54322ebb88145253732b696b344fd1469bd55f
Clement ROULLET <clement.roullet@gmail.com>
2012-09-12 11:35:16 +0200

    Replaced simple quote by double quote to avoid Mysql errors

--- app/models/email_listing.rb
+++ app/models/email_listing.rb
@@ -2,6 +2,6 @@ class EmailListing < ActiveRecord::Base
   attr_accessible :name, :per_line, :countries
 
   def countries_to_sql
-    countries.split(";").map{|m| "'#{m}'"}.join(",")
+    countries.split(";").map{|m| "\"#{m}\""}.join(",")
   end
 end
