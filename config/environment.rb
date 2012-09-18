cb01194969d3485573fb97b891935202effa9168
Clement ROULLET <clement.roullet@gmail.com>
2012-09-18 11:23:39 +0200

    Added configuration file loading

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+TUNA_CONFIG = YAML.load_file("#{Rails.root}/config/tuna.yml")[Rails.env]
 # Initialize the rails application
 CheerfulTuna::Application.initialize!
