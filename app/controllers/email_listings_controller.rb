6d7850eaa73effa7742914a95b10011acd1214ca
Clement ROULLET <clement.roullet@gmail.com>
2012-11-08 21:16:00 +0100

    Disable mailling generation for users

--- app/controllers/email_listings_controller.rb
+++ app/controllers/email_listings_controller.rb
@@ -15,29 +15,14 @@ class EmailListingsController < ApplicationController
   # GET /email_listings/1.json
   def show
     @email_listing = EmailListing.find(params[:id])
-
-    if @email_listing.countries.nil?
-      @listing = []
+    unless current_user.is_admin?
+      @emails = []
     else
-      select_count = "count(emails.address) as list_size"
-
-      countries = ["SELECT emails.address"]
-      countries << "FROM `contacts` LEFT JOIN `emails` ON contacts.id = emails.contact_id"
-      countries << "WHERE ("
-      countries << "contacts.country in (#{@email_listing.countries_to_sql})"
-      countries << "AND contacts.category NOT LIKE 'Sénat%'"
-      countries << "AND contacts.is_active"
-      countries << ")"
-
-      @listing = Contact.find_by_sql(countries.join(" ")).map(&:address).compact.each_slice(@email_listing.per_line).to_a
-      @listing_size = Contact.find_by_sql(countries.join(" ").gsub("emails.address", select_count)).first.list_size
-
-      #@listing = Contact.find(
-        #:all,
-        #select: "emails.address",
-        #conditions: ["contacts.country = ? AND contacts.category NOT LIKE ?", @email_listing.countries,  "Sénat%"],
-        #joins: "LEFT JOIN `emails` ON contacts.id = emails.contact_id").map(&:address).compact.each_slice(@email_listing.per_line).to_a
+      @emails = @email_listing.find_emails
     end
+    @listing = (@emails.nil? ? [] : @emails.map(&:address).compact.each_slice(@email_listing.per_line).to_a)
+
+    @listing_size = @emails.size
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @email_listing }
@@ -58,36 +43,25 @@ class EmailListingsController < ApplicationController
   # GET /email_listings/1/edit
   def edit
     @email_listing = EmailListing.find(params[:id])
-    @countries = @email_listing.countries.split(";")
+    @countries = @email_listing.countries.split(";") rescue []
   end
 
   # POST /email_listings
   # POST /email_listings.json
   def create
-    begin
-      if params[:country].present?
-        Rails.logger.debug params[:country][:name]
-        countries = params[:country][:name].join(";")
-        Rails.logger.debug countries
-        @email_listing = EmailListing.new(params[:email_listing])
-        @email_listing.update_attribute(:countries, countries)
-      end
-      @email_listing.save
-    rescue ActiveRecord::RecordNotUnique => e
-      if e.message =~ /Duplicate entry/
-        flash[:error] = "Sorry, a mailing with the same options already exists. Please try again."
-      else
-        flash[:error] = "Sorry, something went wrong: #{e.message}."
-      end
-    end
+    params[:email_listing][:tags] = params[:email_listing][:tags].sort.first if params[:email_listing][:tags].present?
+    params[:email_listing][:countries] = params[:email_listing][:countries].sort.join(",") if params[:email_listing][:countries].present?
+    @email_listing = EmailListing.new(params[:email_listing])
+    #  @email_listing.update_attribute(:countries, countries)
 
     respond_to do |format|
-      unless flash[:error]
+      if @email_listing.save
         format.html { redirect_to @email_listing, notice: 'Email listing was successfully created.' }
-        format.json { render json: @email_listing, status: :created, location: @email_listing }
+        #format.json { render json: @email_listing, status: :created, location: @email_listing }
       else
+        @countries = params[:email_listing][:countries].split(',') rescue []
         format.html { render action: "new" }
-        format.json { render json: @email_listing.errors, status: :unprocessable_entity }
+        #format.json { render json: @email_listing.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -95,19 +69,19 @@ class EmailListingsController < ApplicationController
   # PUT /email_listings/1
   # PUT /email_listings/1.json
   def update
+    params[:email_listing][:tags] = params[:email_listing][:tags].sort.first if params[:email_listing][:tags].present?
+    params[:email_listing][:countries] = params[:email_listing][:countries].sort.join(",") if params[:email_listing][:countries].present?
+
     @email_listing = EmailListing.find(params[:id])
-    if params[:country].present?
-      countries = params[:country][:name].join(';')
-      @email_listing.update_attribute(:countries, countries)
-    end
 
     respond_to do |format|
       if @email_listing.update_attributes(params[:email_listing])
         format.html { redirect_to @email_listing, notice: 'Email listing was successfully updated.' }
-        format.json { head :no_content }
+        #format.json { head :no_content }
       else
+        @countries = @email_listing.countries.split(',')
         format.html { render action: "edit" }
-        format.json { render json: @email_listing.errors, status: :unprocessable_entity }
+        #format.json { render json: @email_listing.errors, status: :unprocessable_entity }
       end
     end
   end
