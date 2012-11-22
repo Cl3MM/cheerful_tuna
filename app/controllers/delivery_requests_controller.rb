985e3a03f2ed4c350ba19108bbd07d2117cc20d4
Clement ROULLET <clement.roullet@gmail.com>
2012-11-22 20:38:43 +0100

    Work in progress

--- app/controllers/delivery_requests_controller.rb
+++ app/controllers/delivery_requests_controller.rb
@@ -1,11 +1,23 @@
 class DeliveryRequestsController < ApplicationController
 
-  before_filter :authorize_admin!, except: [:new, :create]
+  before_filter :authorize_admin!, except: [:new, :create, :show]
+
+  def delivery_request_pdf
+    @delivery_request_pdf = DeliveryRequest.find(params[:id])
+    respond_to do |format|
+      format.pdf do # show.html.erb
+        pdf = DeliveryRequestPdf.new(@delivery_request)
+        send_data pdf.render, filename: "Delivery_Request_Form_summary.pdf",
+          type: "application/pdf",
+          disposition: "inline"
+      end
+    end
+  end
 
   # GET /delivery_requests
   # GET /delivery_requests.json
   def index
-    session[:ppage] = (params[:per_page].to_i rescue 25) if (params[:per_page] && (not params[:per_page.blank?] ) )
+    session[:ppage] = (params[:per_page].to_i rescue 25) if (params[:per_page] && (not params[:per_page].blank? ) )
     @per_page = session[:ppage].to_s
     params[:ppage] = session[:ppage]
     params.delete :per_page
@@ -24,22 +36,23 @@ class DeliveryRequestsController < ApplicationController
     @delivery_request = DeliveryRequest.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
+      format.html { render 'joomla/delivery_requests/show' }
+      #format.html # show.html.erb
       format.json { render json: @delivery_request }
     end
   end
 
   # GET /delivery_requests/new
   # GET /delivery_requests/new.json
-  def new
-    @technos = DLV_RQST_TECHNOS.keys.each_slice(3).to_a
-    @delivery_request = DeliveryRequest.new
+  #def new
+    #@technos = DLV_RQST_TECHNOS.keys.each_slice(3).to_a
+    #@delivery_request = DeliveryRequest.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @delivery_request }
-    end
-  end
+    #respond_to do |format|
+      #format.html # new.html.erb
+      #format.json { render json: @delivery_request }
+    #end
+  #end
 
   # GET /delivery_requests/1/edit
   def edit
@@ -49,13 +62,15 @@ class DeliveryRequestsController < ApplicationController
   # POST /delivery_requests
   # POST /delivery_requests.json
   def create
+    params[:delivery_request][:user_agent] = Rails.env.test? ? "test_user_agent" : request.env['HTTP_USER_AGENT']
+
     params[:delivery_request][:user_ip] = request.remote_ip
-    params[:delivery_request][:user_agent] = request.env['HTTP_USER_AGENT']
     params[:delivery_request][:referer] = request.env['HTTP_REFERER']
+
     @delivery_request = DeliveryRequest.new(params[:delivery_request])
 
     respond_to do |format|
-      if @delivery_request.save && verify_recaptcha(model: @delivery_request, message: "Sorry, there was an error reading your Captch, please try again!") 
+      if @delivery_request.save!  && (Rails.env.test? or verify_recaptcha(model: @delivery_request, message: "Sorry, there was an error reading your Captch, please try again!"))
 #      if @delivery_request.save
         format.html { redirect_to @delivery_request, notice: 'Delivery request was successfully created.' }
         format.json { render json: @delivery_request, status: :created, location: @delivery_request }
