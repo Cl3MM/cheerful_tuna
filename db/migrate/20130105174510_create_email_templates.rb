502c4becb6db4ec8f9f060235538baf698f70766
Clement ROULLET <clement.roullet@gmail.com>
2013-01-08 09:10:57 +0100

    :droplet: Added Email Templates model

---
+++ db/migrate/20130105174510_create_email_templates.rb
@@ -0,0 +1,11 @@
+class CreateEmailTemplates < ActiveRecord::Migration
+  def change
+    create_table :email_templates do |t|
+      t.string :name
+      t.string :language
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
