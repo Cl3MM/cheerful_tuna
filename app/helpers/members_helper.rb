73c4558ba4e6425cad07c27a9b7a4d3cd806cfbb
Clement ROULLET <clement.roullet@gmail.com>
2013-01-23 18:22:52 +0100

    :scream: Update member_address helper to include address_continued

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -26,15 +26,6 @@ module MembersHelper
     html << "Email: #{contact.email_addresses.map{|e| "#{mail_to(e, e, encode:"hex")}"}.join(", ")}"
     h html.join("<br />")
   end
-  #def contact_details member
-    #html = ["<dl>"]
-    #html << member.contacts.map do |c|
-      #"<dt>#{c.full_name}</dt>
-      #<dd>#{c.email_addresses.map{|e| "#{mail_to(e, e, encode:"hex")}"}.join("<br />")}</dd>"
-    #end
-    #html << "</dl>"
-    #raw html.join()
-  #end
 
   def display_joomla_address member
     html = <<HTML
@@ -49,16 +40,14 @@ HTML
   raw html
   end
   def display_address member
-    html = <<HTML
-<address>
-  <strong>#{h(member.company.upcase)}</strong><br>
-  #{(member.address.blank? ? link_to("Please set the Address", edit_member_path(member)) : h(member.address)) }<br>
-  #{(member.postal_code.blank? ? link_to("Please set the Postal Code", edit_member_path(member)) : member.postal_code ) }, 
-  #{(member.city.blank? ? link_to("Please set the City", edit_member_path(member)) : member.city ) }<br>
-  #{member.country.upcase}<br>
-</address>
-HTML
-  raw html
+    html = [ "<strong>#{h(member.company.upcase)}</strong>" ]
+    html << "#{(member.address.blank? ? link_to("Please set the Address", edit_member_path(member)) : h(member.address)) }"
+    html << "#{member.address_continued}" unless member.address_continued.blank?
+    html << "#{(member.postal_code.blank? ? link_to("Please set the Postal Code", edit_member_path(member)) : member.postal_code ) }, " + \
+            "#{(member.city.blank? ? link_to("Please set the City", edit_member_path(member)) : member.city ) }"
+    html << "#{member.country.upcase}"
+    html << "</address>"
+  raw "<address>" + html.join("<br/>")
   end
   def membership_details member
     html = <<HTML
