87eb7af1979fb46f16fecde8b649b3a942618ab8
Clement ROULLET <clement.roullet@gmail.com>
2013-01-25 16:23:51 +0100

    :dragon_face:Added default status. Set default status to existing members

--- db/migrate/20130124105152_add_status_to_members.rb
+++ db/migrate/20130124105152_add_status_to_members.rb
@@ -1,5 +1,8 @@
 class AddStatusToMembers < ActiveRecord::Migration
   def change
-    add_column :members, :status, :string
+    add_column :members, :status, :string, default: :pending
+    Member.all.each do |m|
+      m.update_attribute :status, :pending
+    end
   end
 end
