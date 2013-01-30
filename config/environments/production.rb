e723fb6cbdb0525cd9629ce67a4c8cc3fe13d03a
Clement ROULLET <clement.roullet@gmail.com>
2013-01-30 13:08:21 +0100

    :tropical_fish: Changed Log level to info, reduce log size before rotation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,10 +50,10 @@ CheerfulTuna::Application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
-  config.logger = Logger.new( Rails.root.join("log", Rails.env + ".log"), 10, 3*1024*1024 )
+  config.logger = Logger.new( Rails.root.join("log", Rails.env + ".log"), 10, 2*1024*1024 )
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
