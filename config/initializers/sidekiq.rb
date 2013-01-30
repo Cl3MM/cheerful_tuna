2c252b4d99a6c8e319ec412adddf1b9d10698ffa
Clement ROULLET <clement.roullet@gmail.com>
2013-01-30 11:55:13 +0100

    :paw_prints: Add a fix for Rails 3.2 to set DB pooling

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,12 @@
 Sidekiq.configure_server do |config|
   #config.redis = { :url => 'redis://redis.example.com:7372/12', :namespace => 'mynamespace' }
   config.redis = { url: "redis://#{ENVIRONMENT_CONFIG[:redis_socket]}", namespace: "#{Rails.env}-#{Rails.application.class.to_s.downcase}" }
+
+  database_url = ENV['DATABASE_URL']
+  if(database_url)
+    ENV['DATABASE_URL'] = "#{database_url}?pool=30"
+    ActiveRecord::Base.establish_connection
+  end
 end
 
 Sidekiq.configure_client do |config|
