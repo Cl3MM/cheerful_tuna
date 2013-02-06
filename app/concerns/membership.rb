5754df1fbe972b5972a8b7c8809168de0c318ced
Clement ROULLET <clement.roullet@gmail.com>
2013-02-06 18:48:42 +0100

    :pound: Added Membership Concern

--- app/concerns/membership.rb
+++ app/concerns/membership.rb
@@ -1,7 +1,30 @@
 module Membership
-  def self.included(base)
-    base.attr_accessible :subscriptions_attributes
-    base.has_many :subscriptions, as: :owner, dependent: :destroy, inverse_of: :owner
-    base.accepts_nested_attributes_for :subscriptions
+  extend ActiveSupport::Concern
+
+  included do
+    before_save :set_current_subscription
+    has_many    :subscriptions, as: :owner, dependent: :destroy, inverse_of: :owner
+    #attr_accessible :subscriptions_attributes
+    #accepts_nested_attributes_for :subscriptions
   end
+
+  def current_subscription
+    Rails.logger.debug "+" * 120
+  end
+  #def self.included(base)
+    #base.attr_accessible :subscriptions_attributes
+    #base.has_many :subscriptions, as: :owner, dependent: :destroy, inverse_of: :owner
+    #base.accepts_nested_attributes_for :subscriptions
+  #end
+
+  module ClassMethods
+    def mandatory_documents
+      if const_defined?( "#{self.name.pluralize}_membership_documents".upcase )
+        eval "#{self.name.pluralize.upcase}_membership_documents".upcase
+      else
+        {}
+      end
+    end
+  end
+
 end
