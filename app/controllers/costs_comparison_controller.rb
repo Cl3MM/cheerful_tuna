3048e009aa48cfdd9326338826d36a639c11868f
Clement ROULLET <clement.roullet@gmail.com>
2013-02-14 16:45:10 +0100

    :pensive: Remove debug statements

--- app/controllers/costs_comparison_controller.rb
+++ app/controllers/costs_comparison_controller.rb
@@ -1,14 +1,11 @@
 class CostsComparisonController < ApplicationController
   def index
-    debug "#{params}"
     @q = if params[:costs_comparison].present?
                CostsComparison.new(params[:costs_comparison])
              else
                CostsComparison.new
              end
     @q.convert_to_integer if @q.valid?
-    debug "#{@q.ceres_contribution_fee}"
-    debug "#{@q.ceres_max_contribution}"
     render action: :index
   end
 end
